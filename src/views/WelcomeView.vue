fd44b42dccafc8baf898a8a5977ec15e6064cbb6
NR Habibi <nrhbb14@gmail.com>
2024-01-26 11:12:56 +0800

    create a welcomeview

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 class="text-2xl md:text-5xl text-black font-bold">JAMIILAH | <span class="bg-black p-2 text-white ">COPYWRITER</span></h1>
+        <h1 class="text-5xl md:text-8xl text-white font-bold py-5 tracking-wide">PORTOFOLIO</h1>
+        <RouterLink :to="{ name: 'Home'}">
+            <Button class="inline-block">Get Started </Button>
+        </RouterLink>
+        
+    </div> 
+</template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { RouterLink } from 'vue-router';
+</script>
+
+<style scoped>
+
+</style>
