77940e14de9b27fa69dddd6b50da5ee4941ce391
NR Habibi <nrhbb14@gmail.com>
2024-01-26 11:12:47 +0800

    create a footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="FooterMain" class="w-full bg-black">
+        <div class="max-w-[1150px] px-4 mx-auto py-5">
+            <div class="md:flex md:gap-10 justify-between w-full items-center">
+                <div class="md:w-[50%]">
+                    <div class="text-white text-2xl md:text-4xl font-bold">Jamiilah.</div>
+                    <div class="text-white text-lg md:text-xl font-semibold">Copywriter Extraordinaire</div>
+                </div>
+                <div class="md:w-[50%] ">
+                    <div class=" text-lg md:text-xl font-bold text-white">Connect on :</div>
+                    <div class="text-white text-md md:text-lg flex-row">LinkedIn</div>
+                    <div class="text-white text-md md:text-lg flex-row">Instagram</div>
+                </div>
+                <div class="md:w-[50%] ">
+                    <div class="text-lg md:text-xl font-bold text-white">Legal</div>
+                    <div class="text-white text-md md:text-lg flex-row">Term of Use</div>
+                    <div class="text-white text-md md:text-lg flex-row">Privacy Police</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="FooterSub" class="w-full bg-black text-sm text-gray-400">
+        <div class="max-w-[1200px] mx-auto p-4 text-center text-white font-semibold">
+            Copyright&#169;2024 Hbb, vue-shadcn-typescript | All Right Reserved
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
+
+<style lang="scss" scoped></style>
