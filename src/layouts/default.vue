a155075432d66e782ad7753fa288733f1229211f
NR Habibi <nrhbb14@gmail.com>
2024-01-26 11:12:20 +0800

    create a layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <NavBar />
+    </header>
+    <main class="min-h-screen">
+        <RouterView />
+    </main>
+    <footer>
+        <Footer />
+    </footer>
+</template>
+
+<script setup lang="ts">
+import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+
+</style>
