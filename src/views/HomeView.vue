54eb0cff4bbe445157e00555aec0fb697f205849
NR Habibi <nrhbb14@gmail.com>
2024-01-30 17:00:40 +0800

    edit code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
+<template>
+  <div id="home">
+    <Hero />
+  </div>
+  <div id="about">
+    <About />
+  </div>
+  <div id="service">
+    <Skill />
+  </div>
+  <div id="project">
+    <ProjectView />
+  </div>
+  <div id="contact">
+
+  </div>
+
+</template>
+
 <script setup lang="ts">
-// import TheWelcome from '../components/TheWelcome.vue'
+import Hero from "./porto/HeroView.vue"
+import About from "./porto/AboutView.vue";
+import Skill from "./porto/SkillView.vue";
+import ProjectView from "./porto/ProjectView.vue";
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -3,13 +3,17 @@
         <h1 class="text-2xl md:text-5xl text-black font-bold">JAMIILAH | <span class="bg-black p-2 text-white ">COPYWRITER</span></h1>
         <h1 class="text-5xl md:text-8xl text-white font-bold py-5 tracking-wide">PORTOFOLIO</h1>
         <RouterLink :to="{ name: 'Home'}">
-            <Button class="inline-block">Get Started </Button>
+            <Button>
+                Get Started 
+                <Icon icon="material-symbols:keyboard-double-arrow-right" height="24"/>
+            </Button>
         </RouterLink>
         
     </div> 
 </template>
 
 <script setup lang="ts">
+import { Icon } from '@iconify/vue';
 import { Button } from '@/components/ui/button'
 import { RouterLink } from 'vue-router';
 </script>
