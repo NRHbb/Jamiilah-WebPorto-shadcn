ea0513c7e7219a3d8d68ad8201cc40610dd238ef
NR Habibi <nrhbb14@gmail.com>
2024-02-21 11:06:24 +0800

    create a custom layout for different pages

---
+++ src/layouts/custom.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <CustomNav/>
+    </header>
+    <main class="min-h-screen overflow-x-hidden">
+        <RouterView />
+    </main>
+    <footer>
+        <CustomFooter />
+    </footer>
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue'
+import { RouterView } from 'vue-router'
+import CustomNav from '@/components/CustomNav.vue';
+</script>
+
+<style scoped>
+
+</style>
